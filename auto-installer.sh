a1889ca18b12f156bb3b987fc902c4871b8fa530
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-17 00:56:10 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Configuración de variables de entorno
-HOSTNAME="my-arch-system"   # nombre de host para el sistema
-ROOT_PASSWORD="myrootpwd"   # contraseña para la cuenta root
-USERNAME="myuser"           # nombre de usuario para la cuenta de usuario
-USER_PASSWORD="mypwd"       # contraseña para la cuenta de usuario
-TIMEZONE="America/New_York" # zona horaria para el sistema
-LOCALE="en_US.UTF-8"        # configuración regional del sistema
-DISK="/dev/sda"             # dispositivo de almacenamiento donde se instalará Arch
-
 # Verificación de los requisitos del sistema
 if [[ $(id -u) -ne 0 ]]; then
     echo "Este script debe ser ejecutado como root"
@@ -20,49 +11,52 @@ if [[ ! -d /sys/firmware/efi/efivars ]]; then
     exit 1
 fi
 
+# Preguntar al usuario la cantidad de almacenamiento que se debe utilizar para la partición raíz
+read -p "Introduzca la cantidad de almacenamiento que desea para la partición raíz (/) (ejemplo: 30G, 1T): " ROOT_SIZE
+
 # Crear particiones y formatearlas
 parted --script ${DISK} \
     mklabel msdos \
-    mkpart primary ext4 1MiB 50% \
-    mkpart primary ext4 50% 100% \
+    mkpart primary ext4 1MiB ${ROOT_SIZE} \
+    mkpart primary linux-swap ${ROOT_SIZE} 100% \
     set 1 boot on
 
 mkfs.ext4 ${DISK}1
-mkfs.ext4 ${DISK}2
+mkswap ${DISK}2
+swapon ${DISK}2
 
 # Montar particiones
 mount ${DISK}1 /mnt
-mkdir /mnt/home
-mount ${DISK}2 /mnt/home
 
 # Configuración de la zona horaria
-ln -sf /usr/share/zoneinfo/${TIMEZONE} /mnt/etc/localtime
-hwclock --systohc --utc --noadjfile
+timedatectl set-ntp true
+timedatectl set-timezone America/New_York
 
-# Configuración regional
-echo "${LOCALE} UTF-8" >> /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
-echo "LANG=${LOCALE}" > /mnt/etc/locale.conf
-
-# Configuración del nombre de host
-echo "${HOSTNAME}" > /mnt/etc/hostname
-
-# Configuración de la contraseña de root
-echo "root:${ROOT_PASSWORD}" | arch-chroot /mnt chpasswd
-
-# Instalación del sistema base y del cargador de arranque GRUB
-pacstrap /mnt base
+# Instalación de paquetes del sistema base y del cargador de arranque GRUB
+pacstrap /mnt base linux linux-firmware
 pacstrap /mnt grub
+
+# Generar fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Configuración del cargador de arranque GRUB
 arch-chroot /mnt grub-install --recheck ${DISK}
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
+# Configuración del nombre de host
+echo "Arch_Ayoub" > /mnt/etc/hostname
+
+# Configuración de la contraseña de root
+echo "root:at15Passw0rd" | arch-chroot /mnt chpasswd
+
 # Configuración de la cuenta de usuario
-arch-chroot /mnt useradd -m -G wheel -s /bin/bash ${USERNAME}
-echo "${USERNAME}:${USER_PASSWORD}" | arch-chroot /mnt chpasswd
+arch-chroot /mnt useradd -m -G wheel -s /bin/bash Ayoub
+echo "Ayoub:at15Passw0rd" | arch-chroot /mnt chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
 # Desmontar particiones
 umount -R /mnt
+swapoff ${DISK}2
 
 # Mostrar mensaje de advertencia
 echo "¡Instalación completada! Recuerde retirar el disco o USB antes de encender el sistema."
