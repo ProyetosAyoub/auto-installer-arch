c222b743baad7a88156585d8d28f96c8b6c84d46
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-16 16:45:48 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -1,45 +1,4 @@
-#!/bin/bash
-
-# Configuración del idioma y del teclado
-echo "es_AR.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=es_AR.UTF-8" > /etc/locale.conf
-echo "KEYMAP=es" > /etc/vconsole.conf
-
-# Configuración de la zona horaria
-ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
-hwclock --systohc
-
-# Configuración de la red
-echo "nombre_equipo" > /etc/hostname
-echo "127.0.0.1	localhost" > /etc/hosts
-echo "::1		localhost" >> /etc/hosts
-echo "127.0.1.1	nombre_equipo.localdomain	nombre_equipo" >> /etc/hosts
-systemctl enable dhcpcd.service
-
-# Configuración del gestor de arranque
-pacman -S grub
-grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Configuración de la contraseña del usuario root
-passwd
-
-# Configuración del usuario
-useradd -m usuario
-passwd usuario
-usermod -aG wheel,audio,video,optical,storage usuario
-
-# Instalación de paquetes básicos
-pacman -S xorg-server xorg-xinit xfce4 xfce4-goodies lightdm lightdm-gtk-greeter firefox
-
-# Activación del gestor de inicio
-systemctl enable lightdm.service
-
-# Fin de la instalación
-echo "Instalación finalizada. Por favor reinicie el equipo."
-
-Instalacion completo de arch mediante script.
+#Instalacion completo de arch mediante script.
 
 #!/bin/bash
 
@@ -51,7 +10,7 @@ fi
 
 # Configurar el idioma y el teclado
 echo "Configurando el idioma y el teclado..."
-echo "es_AR.UTF-8 UTF-8" > /etc/locale.gen
+echo "es_ES.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=es_AR.UTF-8" > /etc/locale.conf
 echo "KEYMAP=es" > /etc/vconsole.conf
@@ -59,7 +18,7 @@ echo ""
 
 # Configurar la zona horaria
 echo "Configurando la zona horaria..."
-ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Spain/Madrid /etc/localtime
 hwclock --systohc
 echo ""
 
