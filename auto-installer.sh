8303f3ed5bad6b036783e6df18fb5bb4a4e2a9bb
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-17 17:26:44 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -1,70 +1,76 @@
 #!/bin/bash
+#!/bin/bash
 
-# Mensaje de bienvenida
-echo "Bienvenido a la instalación automática de Arch Linux UEFI"
+# Set up the disk
+echo "Enter the disk to install Arch Linux to (e.g. /dev/sda):"
+read disk
 
-# Solicitar la cantidad de particiones a crear
-read -p "¿Cuántas particiones desea crear? " num_partitions
+sgdisk -Z ${disk}
+sgdisk -og ${disk}
+sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System Partition" ${disk}
 
-# Si no se ingresó un número, salir del script
-if ! [[ "$num_partitions" =~ ^[0-9]+$ ]]; then
-    echo "Debe ingresar un número. Saliendo del script."
-    exit 1
-fi
+echo "Enter the size for the root partition (e.g. 30G):"
+read root_size
 
-# Solicitar el tamaño de la partición swap
-read -p "Ingrese el tamaño de la partición swap en MB (por defecto: 4096MB): " swap_size
+sgdisk -n 2:0:+${root_size} -t 2:8300 -c 2:"Root Partition" ${disk}
 
-# Si no se ingresó un tamaño de partición swap, usar el valor predeterminado
-if [[ -z "$swap_size" ]]; then
-    swap_size=4096
-fi
+echo "Enter the size for the swap partition (e.g. 4G):"
+read swap_size
 
-# Formatear el disco
-echo "Formateando el disco..."
-(
-  echo o;
-  echo n;
-  echo;
-  echo;
-  for ((i=1;i<num_partitions;i+=1)); do
-    echo n;
-    echo;
-    echo;
-  done
-  echo;
-  echo t;
-  echo 1;
-  echo 1;
-  for ((i=2;i<=num_partitions;i+=1)); do
-    echo t;
-    echo $i;
-    echo $i;
-  done
-  echo w;
-) | fdisk /dev/sda
+sgdisk -n 3:0:+${swap_size} -t 3:8200 -c 3:"Swap Partition" ${disk}
 
-# Crear y activar la partición swap
-echo "Creando partición swap..."
-mkswap /dev/sda2
-swapon /dev/sda2
+sgdisk -p ${disk}
 
-# Formatear las particiones y montar el sistema de archivos
-echo "Formateando las particiones..."
-for ((i=1;i<=num_partitions;i+=1)); do
-    mkfs.ext4 /dev/sda$i
-    mkdir /mnt/part$i
-    mount /dev/sda$i /mnt/part$i
-done
+# Format the partitions
+mkfs.fat -F32 ${disk}1
+mkfs.ext4 ${disk}2
+mkswap ${disk}3
 
-# Instalar el sistema base y generar el archivo fstab
-echo "Instalando el sistema base..."
-pacstrap /mnt base linux linux-firmware
+# Mount the partitions
+mount ${disk}2 /mnt
+mkdir /mnt/boot
+mount ${disk}1 /mnt/boot
+swapon ${disk}3
+
+# Install Arch Linux
+pacstrap /mnt base base-devel linux linux-firmware
+
+# Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Copiar el script de postinstalación al sistema instalado
-cp postinstall.sh /mnt
+# Chroot into the new system
+arch-chroot /mnt
 
-# Chroot al sistema instalado y ejecutar el script de postinstalación
-echo "Chroot al sistema instalado y ejecutando el script de postinstalación..."
-arch-chroot /mnt /bin/bash -c "/postinstall.sh"
+# Set the time zone
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+
+# Set the locale
+sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Set the hostname
+echo "Enter the hostname for the system:"
+read hostname
+
+echo "${hostname}" >> /etc/hostname
+
+# Set the root password
+echo "Set the root password:"
+passwd
+
+# Install and configure the bootloader
+pacman -S grub efibootmgr
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+
+sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Exit the chroot and unmount the partitions
+exit
+
+umount -R /mnt
+
+echo "Arch Linux has been installed successfully. You may now remove the installation media and reboot the system."
