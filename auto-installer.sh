8f38defa2c328082f5587060b6f36de878f4fd6f
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-21 11:54:57 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -1,76 +1,74 @@
 #!/bin/bash
 
-# Mostrar mensaje de bienvenida
-echo "Bienvenido al script de instalación de Arch Linux"
+# Verificar la conexión a Internet
+ping -c 3 google.com
 
-# Preguntar al usuario si desea eliminar las particiones previas
-echo "¿Desea eliminar las particiones previas? (s/n)"
-read eliminar_particiones_previas
+# Configurar la distribución de teclado para España
+loadkeys es
 
-if [[ $eliminar_particiones_previas == "s" ]]; then
-  # Eliminar las particiones previas
-  echo "Eliminando las particiones previas"
-  umount -R /mnt
-  wipefs -a /dev/sda
-  echo "Particiones previas eliminadas"
-fi
-
-# Mostrar las particiones actuales
-echo "Particiones actuales:"
+# Verificar si la unidad de disco es la correcta (/dev/sda en este caso)
 lsblk
 
-# Preguntar por la partición root
-echo "Ingrese la partición para el sistema raíz (/):"
-read particion_root
+# Crear partición para boot de 1GB
+echo -e "n\np\n1\n\n+1G\nw" | fdisk /dev/sda
+mkfs.ext4 /dev/sda1
 
-# Validar la partición root
-while ! [[ -e $particion_root ]]; do
-  echo "La partición ingresada no existe. Ingrese una partición válida:"
-  read particion_root
-done
+# Crear partición para swap de 2GB
+echo -e "n\np\n2\n\n+2G\nw" | fdisk /dev/sda
+mkswap /dev/sda2
+swapon /dev/sda2
 
-# Preguntar por la partición de intercambio (swap)
-echo "Ingrese la partición para la memoria de intercambio (swap):"
-read particion_swap
+# Crear partición para root de 100GB
+echo -e "n\np\n3\n\n+100G\nw" | fdisk /dev/sda
+mkfs.ext4 /dev/sda3
 
-# Validar la partición de intercambio (swap)
-while ! [[ -e $particion_swap ]]; do
-  echo "La partición ingresada no existe. Ingrese una partición válida:"
-  read particion_swap
-done
+# Crear partición para home con el resto del espacio disponible
+echo -e "n\np\n4\n\n\nw" | fdisk /dev/sda
+mkfs.ext4 /dev/sda4
 
-# Formatear las particiones
-echo "Formateando particiones"
-mkfs.ext4 $particion_root
-mkswap $particion_swap
-swapon $particion_swap
-echo "Particiones formateadas"
+# Montar particiones
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir /mnt/home
+mount /dev/sda4 /mnt/home
 
-# Montar la partición root
-echo "Montando partición root"
-mount $particion_root /mnt
-echo "Partición root montada"
-
-# Instalar el sistema base
-echo "Instalando sistema base"
-pacstrap /mnt base base-devel
-echo "Sistema base instalado"
-
-# Generar el archivo fstab
-echo "Generando archivo fstab"
+# Actualizar el archivo fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-echo "Archivo fstab generado"
 
-# Copiar el script post-instalación
-echo "Copiando el script post-instalación"
-cp postinstall.sh /mnt
-echo "Script post-instalación copiado"
+# Configurar el idioma para España
+echo "es_ES.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+echo "LANG=es_ES.UTF-8" > /mnt/etc/locale.conf
 
-# Cambiar al sistema instalado
-echo "Cambie al nuevo sistema usando 'arch-chroot /mnt' y ejecute el script post-instalación después de eso."
-echo "¿Desea reiniciar el sistema ahora? (s/n)"
-read reiniciar
+# Configurar la zona horaria para España
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+arch-chroot /mnt hwclock --systohc
 
-if [[ $reiniciar == "s" ]]; then
-  reboot
-fi
+# Configurar el nombre del equipo
+echo "myhostname" > /mnt/etc/hostname
+echo "127.0.0.1 localhost" >> /mnt/etc/hosts
+echo "::1 localhost" >> /mnt/etc/hosts
+echo "127.0.1.1 myhostname.localdomain myhostname" >> /mnt/etc/hosts
+
+# Instalar y configurar el gestor de arranque GRUB
+arch-chroot /mnt pacman -S grub
+arch-chroot /mnt grub-install /dev/sda
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+# Configurar el kernel de carga inicial
+echo "default arch" > /mnt/boot/loader/loader.conf
+echo "timeout 3" >> /mnt/boot/loader/loader.conf
+
+# Instalar NetworkManager
+arch-chroot /mnt pacman -S networkmanager
+
+# Habilitar NetworkManager
+arch-chroot /mnt systemctl enable NetworkManager.service
+
+# Salir del arch-chroot y limpiar el sistema
+umount -R /mnt
+rmdir /mnt/boot
+rmdir /mnt/home
+
+echo "La instalación ha finalizado con éxito"
