574a97a83608b6398e1d0d2af9b89dfaf9d97e07
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-21 23:02:49 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -72,8 +72,9 @@ echo "ayoub ALL=(ALL) ALL" >> /etc/sudoers
 
 # Instalar el cargador de arranque
 pacman -S grub
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=i386-pc /dev/sda
+sed -i 's|GRUB_CMDLINE_LINUX_DEFAULT=".*"|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"|' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cf
 
 #Indicamos el kernel de carga inicial
 mkinitcpio -p linux
