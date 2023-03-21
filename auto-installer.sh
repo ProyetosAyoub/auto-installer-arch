7075f27792007c973d99ccff0afbc66e88b33f34
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-21 01:32:51 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -1,77 +1,71 @@
 #!/bin/bash
 
-# Preguntamos al usuario si desea eliminar particiones existentes
-read -p "¿Desea eliminar particiones existentes? (s/n): " eliminar
+echo "¡Bienvenido al script de instalación de Arch Linux!"
 
-if [ "$eliminar" == "s" ]; then
-    # Eliminamos particiones existentes
-    umount -R /mnt
-    wipefs -a /dev/sda
+# Preguntar si se desean eliminar particiones antiguas
+echo "¿Desea eliminar particiones antiguas? (s/n)"
+read answer
+if [ "$answer" == "s" ]
+then
+  echo "Eliminando particiones antiguas..."
+  wipefs -af /dev/sda
 fi
 
-# Pedimos al usuario que introduzca el tamaño en GB para la partición de /boot
-read -p "Introduce el tamaño en GB para la partición /boot: " tamaño_boot
+# Preguntar por las particiones necesarias
+echo "A continuación, deberá ingresar los datos para las particiones necesarias."
+echo "Recuerde que el tamaño de la partición de boot será de 1G, la de swap será de 2G, y la de raíz será el resto del almacenamiento."
+echo "Ingrese el nombre del disco en el que desea crear las particiones (ej. /dev/sda): "
+read disk
 
-# Pedimos al usuario que introduzca el tamaño en GB para la partición de swap
-read -p "Introduce el tamaño en GB para la partición swap: " tamaño_swap
+echo "Creando partición para boot..."
+echo "n
+p
+1
 
-# Pedimos al usuario que introduzca el tamaño en GB para la partición de root
-read -p "Introduce el tamaño en GB para la partición root: " tamaño_root
++1G
+w" | fdisk $disk
 
-# Creamos partición para /boot
-parted --script /dev/sda \
-    mklabel gpt \
-    mkpart primary ext4 1MiB ${tamano_boot}GiB \
-    set 1 boot on
+echo "Creando partición para swap..."
+echo "n
+p
+2
 
-# Creamos partición para swap
-parted --script /dev/sda \
-    mkpart primary linux-swap ${tamano_boot}GiB ${tamano_swap}GiB
++2G
+t
+2
+82
+w" | fdisk $disk
 
-# Creamos partición para root
-parted --script /dev/sda \
-    mkpart primary ext4 ${tamano_swap}GiB ${tamano_swap + tamano_root}GiB
+echo "Creando partición para raíz..."
+echo "n
+p
+3
 
-# Formateamos las particiones
-mkfs.ext4 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
 
-# Activamos swap
-swapon /dev/sda2
+w" | fdisk $disk
 
-# Montamos la partición de root
-mount /dev/sda3 /mnt
+# Formatear particiones
+mkfs.fat -F32 ${disk}1
+mkswap ${disk}2
+swapon ${disk}2
+mkfs.ext4 ${disk}3
 
-# Instalamos el sistema base
-pacstrap /mnt base linux linux-firmware
+# Montar particiones
+mount ${disk}3 /mnt
+mkdir /mnt/boot
+mount ${disk}1 /mnt/boot
 
-# Generamos el archivo fstab
+# Instalar sistema base y network manager
+pacstrap /mnt base linux linux-firmware networkmanager
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Copiamos el script de post-instalación al sistema que acabamos de instalar
-cp post_installation_script.sh /mnt
+# Configurar el bootloader
+arch-chroot /mnt bash -c "pacman -S grub efibootmgr --noconfirm && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg"
 
-# Chroot al sistema que acabamos de instalar
-arch-chroot /mnt /bin/bash << EOF
-# Instalamos y configuramos el bootloader GRUB
-pacman -S grub efibootmgr dosfstools os-prober mtools
-mkdir /boot/efi
-mount /dev/sda1 /boot/efi
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Descargamos y habilitamos el network manager
-pacman -S networkmanager
-systemctl enable NetworkManager
-EOF
-
-# Eliminamos el script de post-instalación
-rm /mnt/post_installation_script.sh
-
-# Preguntamos al usuario si desea reiniciar el sistema
-read -p "¿Desea reiniciar el sistema? (s/n): " reiniciar
-
-if [ "$reiniciar" == "s" ]; then
-    reboot
+# Preguntar si se desea reiniciar
+echo "La instalación ha finalizado. ¿Desea reiniciar el sistema? (s/n)"
+read answer
+if [ "$answer" == "s" ]
+then
+  reboot
 fi
