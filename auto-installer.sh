133ee0d81d444c639ab7ef4c01061414cb806ce0
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-22 10:40:01 +0100

    Update auto-installer.sh

--- auto-installer.sh
+++ auto-installer.sh
@@ -11,31 +11,36 @@ lsblk
 
 # Crear partición para boot de 1GB
 echo -e "n\np\n1\n\n+1G\nw" | fdisk /dev/sda
-mkfs.ext4 /dev/sda1
+mkfs.ext2 /dev/sda1
 parted /dev/sda set 1 boot on
+
 # Crear partición para swap de 2GB
 echo -e "n\np\n2\n\n+2G\nw" | fdisk /dev/sda
 mkswap /dev/sda2
 swapon /dev/sda2
 
-# Crear partición para home con el resto del espacio disponible
-echo -e "n\np\n3\n\n\nw" | fdisk /dev/sda
+# Crear partición para raiz de 40GB
+echo -e "n\np\n3\n\n+40G\nw" | fdisk /dev/sda
 mkfs.ext4 /dev/sda3
+
+# Crear partición para home con el resto del espacio disponible
+echo -e "n\np\n4\n\n\nw" | fdisk /dev/sda
+mkfs.ext4 /dev/sda4
 
 # Montar particiones
 mount /dev/sda3 /mnt
 mkdir /mnt/boot /mnt/var
 mount /dev/sda1 /mnt/boot
 mkdir /mnt/home
-mount /dev/sda3 /mnt/home
+mount /dev/sda4 /mnt/home
 
 echo "Ya estan montaldas las particiones"
 
 #Instalamos el sistema
 
-pacstrap /mnt base linux linux-firmware base-devel
-pacman -Sy archlinux-keyring
-pacstrap /mnt grub-bios
+pacstrap /mnt base linux linux-firmware base-devel nano networkmanager dhcpcd grub
+pacstrap /mnt netctl wpa_supplicant dialog
+pacman -Sy archlinux-keyring    
 genfstab -p /mnt >> /mnt/etc/fstab
 
 #Accederemos a la ruta montada
@@ -44,9 +49,9 @@ echo "Ya estas dentro y no hubo ningun problema de momento...."
 
 # Configurar la zona horaria
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
-hwclock --systohc
-
+hwclock -w
 # Configurar el idioma
+echo KEYMAP=es > /etc/vconsole.conf
 echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=es_ES.UTF-8" >> /etc/locale.conf
@@ -59,9 +64,6 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 archayoub.localdomain archayoub" >> /etc/hosts
 
-# Configurar la contraseña del root
-passwd
-
 # Crear un usuario y otorgarle permisos de sudo
 useradd -m -g users -G wheel -s /bin/bash ayoub
 
@@ -71,19 +73,20 @@ passwd ayoub
 echo "ayoub ALL=(ALL) ALL" >> /etc/sudoers
 
 # Instalar el cargador de arranque
-pacman -S grub
+grub-install /dev/sda
 grub-install --target=i386-pc --boot-directory=/mnt/boot /dev/sda
 sed -i 's|GRUB_CMDLINE_LINUX_DEFAULT=".*"|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"|' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-#Indicamos el kernel de carga inicial
-mkinitcpio -p linux
+# Configurar la contraseña del root
+passwd
 
-#NetworkManager instalacion
-pacman -S networkmanager
-systemctl enable NetworkManager
+# Crear un usuario y otorgarle permisos de sudo
+useradd -m -g users -G wheel -s /bin/bash ayoub
 
-umount /mnt/boot
+# Configurar la contraseña del usuario
+passwd ayoub
+
 umount -R /mnt
 
 echo "Ya esta lista la instalacion a disfrutar!"
