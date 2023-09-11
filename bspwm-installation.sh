71f9f9ce0e8d1721bb53bd99c69c03ea0f39a832
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-09-11 18:35:54 +0200

    Update bspwm-installation.sh

--- bspwm-installation.sh
+++ bspwm-installation.sh
@@ -30,5 +30,7 @@ mkdir -p ~/.config/sxhkd
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 
-# Mensaje final
-echo "La instalación de BSPWM y las aplicaciones adicionales ha finalizado correctamente. Recuerda verificar la configuración y realizar cualquier otro paso manualmente si es necesario."
+# Abrir el archivo sxhkdrc en un editor de texto (puedes cambiar "nano" por tu editor preferido)
+echo "Abriendo sxhkdrc en un editor de texto..."
+nano ~/.config/sxhkd/sxhkdrc  # Puedes cambiar "nano" a tu editor preferido
+
