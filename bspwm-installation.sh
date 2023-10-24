ce76d0152e4756cac8f719020d286dfd13c380b1
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-10-24 11:45:07 +0200

    Update bspwm-installation.sh

--- bspwm-installation.sh
+++ bspwm-installation.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
 # Advertencia al usuario
-echo "Este script instalará paquetes y realizará cambios en la configuración del sistema. Continuar? (y/n)"
+echo "Este script realizará cambios en la configuración del sistema. Continuar? (y/n)"
 read answer
 if [ "$answer" != "y" ]; then
     echo "Abortando instalación."
     exit 1
 fi
 
-# Definir variables para programas y paquetes
-WM_PACKAGES="bspwm sxhkd"
-FONT_PACKAGES="ttf-dejavu ttf-liberation noto-fonts"
-AUDIO_PACKAGES="alsa-utils pulseaudio pavucontrol"
+# Definir variables para programas
 UTILS_PACKAGES="xorg xorg-xinit xterm dmenu feh pcmanfm code alacritty"
-POLYBAR_PACKAGE="polybar"
 
 # Instalar paquetes de programas
 echo "Instalando paquetes de programas..."
-sudo pacman -S $WM_PACKAGES && \
-sudo pacman -S $FONT_PACKAGES && \
-sudo pacman -S $AUDIO_PACKAGES && \
-sudo pacman -S $UTILS_PACKAGES && \
-sudo pacman -S $POLYBAR_PACKAGE
+sudo pacman -S $UTILS_PACKAGES
 
 # Configurar BSPWM (reemplaza con tus propias configuraciones)
 echo "Configurando BSPWM..."
@@ -29,8 +21,3 @@ mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/sxhkd
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
-
-# Abrir el archivo sxhkdrc en un editor de texto (puedes cambiar "nano" por tu editor preferido)
-echo "Abriendo sxhkdrc en un editor de texto..."
-nano ~/.config/sxhkd/sxhkdrc  # Puedes cambiar "nano" a tu editor preferido
-
