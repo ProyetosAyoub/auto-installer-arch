59acdb35b57ce1211a1fa8bad31eb8b5b1f5388e
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-10-26 15:27:15 +0200

    Update bspwm-installation.sh

--- bspwm-installation.sh
+++ bspwm-installation.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 # Advertencia al usuario
-echo "Este script instalará BSPWM, SXHKD, y realizará cambios en la configuración del sistema. Continuar? (y/n)"
+echo "Este script instalará paquetes y realizará cambios en la configuración del sistema. Continuar? (y/n)"
 read answer
 if [ "$answer" != "y" ]; then
-    echo "Abortando instalación y configuración de BSPWM y SXHKD."
+    echo "Abortando instalación."
     exit 1
 fi
 
-# Instalar BSPWM y SXHKD
-echo "Instalando BSPWM y SXHKD..."
-sudo pacman -S bspwm sxhkd
+# Definir variables para programas y paquetes
+WM_PACKAGES="bspwm sxhkd"
+FONT_PACKAGES="ttf-dejavu ttf-liberation noto-fonts"
+AUDIO_PACKAGES="alsa-utils pulseaudio pavucontrol"
+UTILS_PACKAGES="xorg xorg-xinit xterm dmenu feh pcmanfm code alacritty"
+POLYBAR_PACKAGE="polybar"
 
-# Configurar BSPWM
+# Instalar paquetes de programas
+echo "Instalando paquetes de programas..."
+sudo pacman -S $WM_PACKAGES && \
+sudo pacman -S $FONT_PACKAGES && \
+sudo pacman -S $AUDIO_PACKAGES && \
+sudo pacman -S $UTILS_PACKAGES && \
+sudo pacman -S $POLYBAR_PACKAGE
+
+# Configurar BSPWM (reemplaza con tus propias configuraciones)
 echo "Configurando BSPWM..."
 mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/sxhkd
