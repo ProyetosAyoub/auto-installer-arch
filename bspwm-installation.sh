d6915b9e803449a79b8cec02772893a707f7821f
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-10-27 13:02:05 +0200

    Update bspwm-installation.sh

--- bspwm-installation.sh
+++ bspwm-installation.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
 # Advertencia al usuario
-echo "Este script instalará paquetes y realizará cambios en la configuración del sistema. Continuar? (y/n)"
+echo "Este script instalará paquetes necesarios para BSPWM y Dmenu. Continuar? (y/n)"
 read answer
 if [ "$answer" != "y" ]; then
     echo "Abortando instalación."
     exit 1
 fi
 
-# Definir variables para programas y paquetes
+# Definir variables para los paquetes necesarios
 WM_PACKAGES="bspwm sxhkd"
-FONT_PACKAGES="ttf-dejavu ttf-liberation noto-fonts"
-AUDIO_PACKAGES="alsa-utils pulseaudio pavucontrol"
-UTILS_PACKAGES="xorg xorg-xinit xterm dmenu feh pcmanfm code alacritty"
-POLYBAR_PACKAGE="polybar"
+UTILS_PACKAGES="xorg dmenu"
 
-# Instalar paquetes de programas
-echo "Instalando paquetes de programas..."
-sudo pacman -S $WM_PACKAGES && \
-sudo pacman -S $FONT_PACKAGES && \
-sudo pacman -S $AUDIO_PACKAGES && \
-sudo pacman -S $UTILS_PACKAGES && \
-sudo pacman -S $POLYBAR_PACKAGE
+# Instalar paquetes necesarios
+echo "Instalando paquetes necesarios..."
+sudo pacman -S $WM_PACKAGES
+sudo pacman -S $UTILS_PACKAGES
 
 # Configurar BSPWM (reemplaza con tus propias configuraciones)
 echo "Configurando BSPWM..."
