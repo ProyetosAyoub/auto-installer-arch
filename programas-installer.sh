488ae31e496f2bdb8ef4f08f438b7c8b8a24cd15
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-11-02 00:31:23 +0100

    Update programas-installer.sh

--- programas-installer.sh
+++ programas-installer.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
 
-# Advertencia al usuario
-echo "Este script instalará paquetes de programas. Continuar? (y/n)"
-read answer
-if [ "$answer" != "y" ]; then
-    echo "Abortando instalación de programas."
-    exit 1
-fi
-
-# Definir variables para programas
-UTILS_PACKAGES="xorg xorg-xinit xterm dmenu feh pcmanfm code alacritty"
-
-# Instalar paquetes de programas
-echo "Instalando paquetes de programas..."
-sudo pacman -S $UTILS_PACKAGES
